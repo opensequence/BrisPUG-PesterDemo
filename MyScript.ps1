9ec7ab4f1ddb4525a70de7ab6c424db3f969c444
Mitchell Gill <mgill@opensequence.net>
2019-02-14 08:05:52 +1000

    Initial Commit

---
+++ MyScript.ps1
@@ -0,0 +1,123 @@
+<#
+.SYNOPSIS
+
+
+.DESCRIPTION
+
+
+
+.NOTES
+AUTHOR  : Mitchell Gill
+CREATED : 12/02/2019
+VERSION : 12/02/2019
+			1.0 - Initial Revision
+
+.INPUTS
+None. This script does not accept input parameters or pipelines
+
+.OUTPUTS
+None. This script does not output parameters
+
+.EXAMPLE
+
+
+#>
+#====================================================================================================
+#                                             Parameters
+#====================================================================================================
+#region Parameters
+
+[CmdletBinding()]
+Param(
+
+    [Parameter(Mandatory = $false)]
+    [string]$InstanceID,
+
+    [Parameter(Mandatory = $false)]
+    [string]$RegionID,
+
+    [Parameter(Mandatory = $false)]
+    [string]$AMIID
+
+)
+
+#Set the error action preference to stop to handle errors as they occur
+$ErrorActionPreference = "Stop"
+
+#Override the verbose preference
+$VerbosePreference = "SilentlyContinue"
+
+#endregion Parameters
+
+#Requires -Modules @{ModuleName='AWSPowerShell.NetCore';ModuleVersion='3.3.428.0'}
+#====================================================================================================
+#                                             Functions
+#====================================================================================================
+#region Functions
+
+#region Find-EC2Instance
+
+function Find-EC2Instance {
+    [CmdletBinding()]
+    Param (
+        [parameter(Mandatory = $True, Position = 1)]
+        [string]
+        $InstanceID,
+        [parameter(Mandatory = $True, Position = 2)]
+        [string]
+        $Region,
+        [parameter(Mandatory = $True, Position = 3)]
+        [string[]]
+        $AMIID
+    )
+    # retrieve EC2Instance Details
+    try {
+        Write-Verbose "START: Retrieving EC2 Details for Instance: $($InstanceID)"
+        $EC2InstanceDetail = Get-EC2Instance -InstanceID $InstanceID -Region $Region
+        Write-Verbose "SUCCESS: Retrieving EC2 Details for Instance: $($InstanceID)"
+    } catch {
+        Throw "ERROR: Retrieving EC2 Details for Instance: $($InstanceID) ErrorMessage: $($_.Exception.Message)"
+    }
+
+    #Compare AMI ID wth provided ID
+    If ($AMIID -contains $($EC2InstanceDetail.Instances.ImageId)) {
+        Write-Verbose "$($EC2InstanceDetail.Instances.ImageId) Matches one of Provided AMIID: $($AMIID)"
+        return $EC2InstanceDetail
+    } else {
+        Write-Verbose "$($EC2InstanceDetail.Instances.ImageId) does NOT Match one of Provided AMIID: $($AMIID)"
+        return $null
+    }
+
+}
+#endregion Find-EC2Instance
+
+#endregion Functions
+
+#====================================================================================================
+#                                          Initialize Code
+#====================================================================================================
+#region Initialize Code
+If(-not $InstanceID){
+    Exit
+}
+
+#endregion Initialize Code
+#====================================================================================================
+#                                             Main Code
+#====================================================================================================
+#region Main Code
+
+#retrieve the AMI ID of the Instance
+try {
+    Write-Output "START: Retrieving EC2 Details for Instance: $($InstanceID)"
+    $MatchingEC2Instance = Find-EC2Instance -InstanceID "$($InstanceID)" -Region $Region -AMIID "$($AMIID)"
+    Write-Output "SUCCESS: Retrieving EC2 Details for Instance: $($InstanceID)"
+} catch {
+    Write-Error "ERROR: Retrieving EC2 Details for Instance: $($InstanceID) ErrorMessage: $($_.Exception.Message)"
+    Exit
+}
+
+
+
+
+#endregion Main Code
